7727f03a799d0c5bd84d98e28c7695f432e2dba5
Marcelo de Bittencourt <tchelopoa@gmail.com>
2020-03-08 12:24:37 -0300

    Adição de todos os arquivos do projeto

---
+++ Calculadora/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+public class Calculadora {
+
+	public int soma(int x, int y) {
+		return x + y;
+	}
+
+	public int subtracao(int x, int y) {
+		return x - y;
+	}
+
+}
---
+++ Calculadora/src/test/java/testes/CalculadoraTeste.java
@@ -0,0 +1,32 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import calculadora.Calculadora;
+
+class CalculadoraTeste {
+
+	static Calculadora c;
+
+	@BeforeAll
+	static void configuracaoInicial() {
+		c = new Calculadora();
+	}
+
+	@DisplayName("Teste de Soma com Resultado Positivo")
+	@Test
+	void testeSomaResultadoPositivo() {
+		assertEquals(4, c.soma(1, 3), "Soma positiva com erro");
+	}
+	
+	@DisplayName("Teste de Subtracao com Resultado Positivo")
+	@Test
+	void testeSubtracaoResultadoPositivo() {
+		assertEquals(1, c.subtracao(5, 4), "Subtracao positiva com erro");
+	}
+	
+}
